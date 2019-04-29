93e3dfeadcb23229d766ce0da71c25335b8aff7b
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-28 21:38:08 -0500

    adding basic setup in TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,12 +3,25 @@ import NewTodoForm from './NewTodoForm'
 import Todo from './Todo'
 
 class TodoList extends Component {
-  render() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: [{
+                text: "This is a Todo",
+                done: false
+            }]
+        }
+    }
+  
+    render() {
+        const todo = this.state.todos.map(todo => <Todo text={todo.text} done={todo.done} />)
     return (
-      <div>
-        <h1>Hello from TodoList</h1>
+      <div className="TodoList">
+        <h1 className="TodoList-Title">Todo List</h1>
+        <p className="TodoList-Subtitle">A Simple React Todo List app</p>
+        {todo}
         <NewTodoForm />
-        <Todo />
+        
       </div>
     )
   }
