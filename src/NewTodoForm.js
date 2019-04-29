f9e5bd2bfef52febdd60f3a4dd3fd4624d7fcb9a
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-28 21:33:03 -0500

    Setting up File structure

---
+++ src/NewTodoForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class NewTodoForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from newTodo</h1>
+      </div>
+    )
+  }
+}
+
+export default NewTodoForm;
